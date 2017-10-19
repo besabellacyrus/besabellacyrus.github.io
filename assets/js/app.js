05c56e99b4454e3f23a7d448fa578a5f95ba45ff
Cyrus Besabella <besabellacyrus@gmail.com>
2017-10-19 08:27:55 +0800

    added bg

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,42 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    home: true,
+    resume: false,
+    projects: false,
+    contact: false,
+    appBg:'appBg1',
+  },
+  methods: {
+    resumeContent: function() {
+        this.home = false;
+        this.projects = false;
+        this.contact = false;
+        this.resume = true;
+        this.appBg = 'appBg2';
+    },
+    homeContent: function() {
+        this.home = true;
+        this.projects = false;
+        this.contact = false;
+        this.resume = false;
+        this.appBg = 'appBg1';
+
+    },
+    contactContent: function() {
+        this.home = false;
+        this.projects = false;
+        this.contact = true;
+        this.resume = false;
+        this.appBg = 'appBg3';
+    },
+    projectsContent: function() {
+        this.home = false;
+        this.projects = true;
+        this.contact = false;
+        this.resume = false;
+        this.appBg = 'appBg4';
+    },
   }
 })
